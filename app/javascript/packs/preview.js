9c8e35431e7d422540cee1b63b0d71b3621bdaa3
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-17 23:10:14 +0900

    投稿画面のプレビュー機能実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,21 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
-  console.log(123);
+  const ImageList = document.getElementById('image-list');
+  document.getElementById('message_image').addEventListener('change', function(e){
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
 });
+}
