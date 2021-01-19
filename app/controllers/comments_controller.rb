430ed0d398aa2b3351ea6acda45cb8c6d5fe6dd7
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-19 18:26:20 +0900

    commentsコントローラーでcreateアクションを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts do
-    resources :comments, only: [:create, :edit, :delete]
+    resources :comments, only: [:create, :destroy]
   end
 end
