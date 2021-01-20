7cc2e2fa421aec532f43360e8ab0a2898e01aeec
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-20 23:55:08 +0900

    検索機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :root_to_path, only: [:edit, :destroy]
 
@@ -43,6 +43,10 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def search
+    @posts = Post.search(params[:keyword])
   end
 
 
