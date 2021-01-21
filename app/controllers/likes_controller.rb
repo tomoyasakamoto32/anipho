2cf21c16aa863b7c8031165a277517170f638ea0
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-21 18:39:17 +0900

    非同期通信でのいいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,16 @@
 class LikesController < ApplicationController
+  before_action :like_params
+
   def create
     Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to root_path
+  end
+
+  private 
+  def like_params
+    @post = Post.find(params[:id])
   end
 end
