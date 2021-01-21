a96678c7bb7cdca6dca3ec39b3c48563e7ee6528
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-21 16:46:08 +0900

    いいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :password, format: { with: PASSWORD_REGEX}
   
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
+  end
 end
