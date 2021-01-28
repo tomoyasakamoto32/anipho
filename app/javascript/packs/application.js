9fa81b9227eb428f3a450e6b075de416368fc8f9
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-28 19:36:51 +0900

    文字数カウント機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('./preview')
 require('./swiper')
+require('./count')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
