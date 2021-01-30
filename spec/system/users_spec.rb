73384b6cf37ac6243e83fb858eadbd2892cacff8
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-30 20:27:22 +0900

    ログインができるときの結合テスト記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -18,16 +18,16 @@ RSpec.describe "ユーザー新規登録", type: :system do
       fill_in 'メールアドレス', with: @user.email
       fill_in 'パスワード', with: @user.password
       fill_in 'パスワード(確認)', with: @user.password_confirmation
-      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      # 新規登録を押すとユーザーモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change {User.count}.by(1)
-      # トップページへ遷移したことを確認する
+      # 新規登録ページへ遷移したことを確認する
       expect(current_path).to eq root_path
       # トップページにログアウトボタンやユーザーの名前が表示されることを確認する
       expect(page).to have_content('ログアウト')
       expect(page).to have_content(@user.nickname)
-      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      # 新規登録ページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
@@ -51,6 +51,35 @@ RSpec.describe "ユーザー新規登録", type: :system do
       }.to change {User.count}.by(0)
       # 新規登録ページへ戻ってくることを確認する
       expect(current_path).to eq "/users"
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページに遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページに遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      # ログインボタンを押す
+        find('input[name="commit"]').click
+      # トップページに遷移する
+      visit root_path
+      # トップページにログアウトボタンやユーザーの名前が表示されることを確認する
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_content(@user.nickname)
+      # 新規登録ページに遷移するボタンやログインページに遷移するボタンがないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
 end
