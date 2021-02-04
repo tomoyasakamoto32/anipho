baba69c24865dc0e79a9e6f4bb8f81eaea97e0d9
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-02-04 18:46:07 +0900

    フォロー機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @posts = user.posts.order('created_at DESC').page(params[:page]).per(12)
+    @post_all = Post.with_attached_images.includes(:user)
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @posts = @post_all.where(user_id: @user).order('created_at DESC').page(params[:page]).per(12)
   end
 end
