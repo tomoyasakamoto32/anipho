20420e7d1470bc40ede28dfde6068ce124c0d219
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-02-09 14:54:58 +0900

    indexアクションのコントローラー単体テスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
+describe PostsController, type: :request do
+
+  before do
+    @post = FactoryBot.create(:post)
+  end
+
+  describe 'GET #index' do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get root_path
+      expect(response.status).to eq 200
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのテキストが存在する' do 
+      get root_path
+      expect(response.body).to include(@post.title)
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do 
+      get root_path
+      expect(response.body).to include('test_image.png')
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
+      get root_path
+      expect(response.body).to include('投稿を検索する')
+    end
+    it 'indexアクションにリクエストするとレスポンスにカテゴリー検索フォームが存在する' do
+      get root_path
+      expect(response.body).to include('カテゴリーで探す')
     end
   end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの情報が存在する' do 
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do 
+    end
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+    end
+  end 
 end
