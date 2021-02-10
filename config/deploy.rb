411d92484f035e5c3c49d3466343044bff0e73c7
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-02-10 15:39:45 +0900

    自動デプロイエラー修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.15.0"
 
 set :application, "anipho"
-set :repo_url, "git@example.com:tomoyasakamoto32/anipho.git"
+set :repo_url, "git@github.com:tomoyasakamoto32/anipho.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
