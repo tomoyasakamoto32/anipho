8bc2b694c408ade407e561d65f7c9df16a3b4575
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-02-19 21:38:13 +0900

    デプロイエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   get 'relationship/:id' => 'relationships#followers', as:'followers'
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
-  get 'likes' => 'likes#index', as:  'likes'
+  get 'likes' => 'likes#index', as: 'likes'
   get '/posts/category/:id' => "posts#category", as: 'post_category'
 end
